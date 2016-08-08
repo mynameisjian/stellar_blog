0f93148b67f9acd22df9b742774d1c1a73047b67
jian <jianjian19841984@gmail.com>
2016-08-08 21:26:14 +1000

    Soft delete added.
    - Soft delete added to blog model.
    - Create new migration file to add a new field deleted_at to users
      table.

--- app/Blog.php
+++ app/Blog.php
@@ -4,9 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Blog extends Model
 {
 
+	use SoftDeletes;
+
 	protected $table = 'blogs';	
+
+	protected $dates = ['deleted_at'];
 
 }
